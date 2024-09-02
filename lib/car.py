eb523d4ff4fb9fa64d378ac8e30afb5ef8fb9502
Nobert-Muma <mumanobert17@gmail.com>
2024-09-02 17:01:21 +0300

    Object Inheritance in Python.

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,9 @@
 from vehicle import Vehicle
 
 class Car(Vehicle):
-    pass
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!" 
+car_1=Car(2344, 12)
+print(car_1.fill_up_tank())
+print(car_1.wheel_number)
+print(car_1.wheel_size)
